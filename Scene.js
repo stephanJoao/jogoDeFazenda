cbc4116605875b1505fb8745d89d67a06f99e918
Igor Knop <igorknop@gmail.com>
2019-08-26 20:10:23 -0300

    Adiciona limpar e mover cena

---
+++ Scene.js
@@ -0,0 +1,32 @@
+function Scene(params) {
+    var exemplo ={
+        sprites: [],
+        ctx: null,
+        w: 300,
+        h: 300
+    }
+    Object.assign(this, exemplo, params);
+}
+
+Scene.prototype = new Scene();
+Scene.prototype.constructor = Scene;
+
+Scene.prototype.adicionar = function(sprite){
+    this.sprites.push(sprite);
+};
+
+Scene.prototype.desenhar = function(){
+    for(var i = 0; i<this.sprites.length; i++){
+        this.sprites[i].desenhar(this.ctx);
+    }  
+};
+
+Scene.prototype.mover = function(dt){
+    for(var i = 0; i<this.sprites.length; i++){
+        this.sprites[i].mover(dt);
+    }  
+};
+
+Scene.prototype.limpar = function(){
+    this.ctx.clearRect(0,0, this.w, this.h);
+}
