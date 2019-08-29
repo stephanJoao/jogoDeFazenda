5036d275a2b37c8306e2cc8a306301447a45af41
Igor Knop <igorknop@gmail.com>
2019-08-29 20:19:29 -0300

    Adiciona movimento no sentido

--- Sprite.js
+++ Sprite.js
@@ -8,6 +8,7 @@ function Sprite(params = {}) {
         w: 10,
         a: 0,
         va: 0,
+        vm: 0,
         color: "blue",
         imune: 0,
         atirando: 0,
@@ -19,14 +20,15 @@ Sprite.prototype = new Sprite();
 Sprite.prototype.constructor = Sprite;
 
 Sprite.prototype.desenhar = function(ctx) {
+    
     ctx.save();
     ctx.translate(this.x, this.y);
+    ctx.strokeRect(-this.w/2, -this.h/2, this.w, this.h);
     ctx.rotate(this.a);
     ctx.fillStyle = this.color;
     ctx.strokeStyle = "black";
     ctx.lineWidth = 1;
 
-//    ctx.strokeRect(-this.w/2, -this.h/2, this.w, this.h);
 
     ctx.beginPath();
     ctx.moveTo(-this.w/2, -this.h/2);
@@ -40,7 +42,11 @@ Sprite.prototype.desenhar = function(ctx) {
 };
 
 Sprite.prototype.mover = function (dt) {
+    this.a = this.a + this.va*dt;
+
+    this.vx = this.vm*Math.cos(this.a);
+    this.vy = this.vm*Math.sin(this.a);
+
     this.x = this.x + this.vx*dt;
     this.y = this.y + this.vy*dt;
-    this.a = this.a + this.va*dt;
 }
