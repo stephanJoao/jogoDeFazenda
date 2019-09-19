7a1878371c5a9ab448c0673a38fa2b3f19e7ad23
Igor Knop <igorknop@gmail.com>
2019-09-19 20:09:16 -0300

    Adiciona o AssetsManager

---
+++ AssetsManager.js
@@ -0,0 +1,18 @@
+function AssetsManager() {
+    this.aCarregar = 0;
+    this.carregadas = 0;
+    this.assets = {};
+}
+
+AssetsManager.prototype.loadImage = function (key, url) {
+    console.log(`Carregando imagem ${url}...`);
+
+    this.aCarregar++;
+    var imagem = new Image();
+    console.log(`Imagem ${key}: ${url} carregada.`);
+    imagem.src = url;
+    this.assets[key] = imagem;
+    imagem.addEventListener("load", function () {
+        this.carregadas++;
+    });
+}
